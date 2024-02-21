1e4a1e6577e2ce3d78905fbd52b64abf4211f408
xudq <xudingqiang520@126.com>
2024-02-21 16:29:49 +0800

    fix some size

---
+++ app/src/main/java/com/boringdroid/systemui/receiver/UninstallReceiver.java
@@ -0,0 +1,23 @@
+package com.boringdroid.systemui.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.boringdroid.systemui.utils.CollectUtils;
+import com.boringdroid.systemui.utils.LogTools;
+
+public class UninstallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("bella","--------UninstallReceiver---------");
+        if (intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
+            String packageName = intent.getData().getEncodedSchemeSpecificPart();
+            LogTools.Companion.i("packageName "+packageName + " ,getPackageName "+context.getPackageName());
+            if (packageName.equals(context.getPackageName())) {
+                CollectUtils.deleteCollectData(context,packageName);
+            }
+        }
+    }
+}
